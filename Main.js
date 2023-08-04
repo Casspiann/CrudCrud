39d30e35887decc22b629c57a9979db66f74b946
Casspiann <tarunrana4221>
2023-08-04 22:25:32 +0530

    add file

--- Main.js
+++ Main.js
@@ -1,32 +1,50 @@
 //All method localstorage and session storage methods are same
 var item = document.getElementById("Submit");
  //cokiees
- document.cookie = 'firstName=john; expire='+new Date(2020,0,1).toUTCString();
-    console.log(document.cookie);
+ //document.cookie = 'firstName=john; expires=' + new Date(2023, 0, 1).toUTCString();
+//console.log(document.cookie);
+//console.log(123);
 
 item.addEventListener('click',printDetail);
 function printDetail(e){
     
     var firstName = document.getElementById("firstName").value;
-    localStorage.setItem("firstName",firstName);
-    console.log(localStorage.getItem('firstName'));
+   // localStorage.setItem("firstName",firstName);
+    //console.log(localStorage.getItem('firstName'));
     var lastName = document.getElementById("lastName").value;
-    localStorage.setItem("lastName",lastName);
-    console.log(localStorage.getItem('lastName'));
+   // localStorage.setItem("lastName",lastName);
+   // console.log(localStorage.getItem('lastName'));
     var Gender = document.getElementById("gender").value;
-    localStorage.setItem("Gender",Gender);
-    console.log(localStorage.getItem('Gender'));
+    //localStorage.setItem("Gender",Gender);
+    //console.log(localStorage.getItem('Gender'));
 
     var Email  = document.getElementById("email").value;
-    localStorage.setItem("Email",Email);
-    console.log(localStorage.getItem('Email'));
+    //localStorage.setItem("Email",Email);
+    //console.log(localStorage.getItem('Email'));
     var Password = document.getElementById("Password").value;
-    localStorage.setItem("password",Password);
-    console.log(localStorage.getItem('password'));
+    //localStorage.setItem("password",Password);
+    //console.log(localStorage.getItem('password'));
     var phoneNumber = document.getElementById("Phone").value;
-    localStorage.setItem("Phone.no",phoneNumber);
-    console.log(localStorage.getItem('Phone.no'));
-    console.log(firstName+" "+lastName+" "+Gender+" "+Email+" "+Password+" "+phoneNumber)
+    //localStorage.setItem("Phone.no",phoneNumber);
+    //console.log(localStorage.getItem('Phone.no'));
+    let myobj = {
+        FirstName:firstName,
+        LastName:lastName,
+        Gender:Gender
+    };
+    let myobject ={
+        Email:Email,
+        Password:Password,
+        PhoneNumber:phoneNumber
+    };
+    let myobjSerialize = JSON.stringify(myobj);
+    localStorage.setItem('myobj',myobjSerialize);
+    let myObjectSerialize = JSON.stringify(myobject);
+    localStorage.setItem('myobject',myObjectSerialize);
+    //console.log(myObjectSerialize);
+    //console.log(myobjSerialize);
+
+   // console.log(firstName+" "+lastName+" "+Gender+" "+Email+" "+Password+" "+phoneNumber)
    
    
 
